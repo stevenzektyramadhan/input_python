9418fd115e8efa4f1142426a1ccdb6f3401a6c5a
stevenzektyramadhan <ldpo7471@gmail.com>
2024-10-25 20:05:18 +0700

    test

---
+++ index.py
@@ -0,0 +1,14 @@
+# jum'at 25 oktober 2024
+
+name = input("masukan nama anda: ")
+
+print(f"hai {name}, selamat datang di ....")
+
+
+# Kalkulator
+
+panjang = int(input("Masukan panjang: "))
+lebar = int(input("Masukan lebar: "))
+hasil = panjang * lebar
+
+print(f"luas = {panjang} * {lebar} = {hasil}")
